78b7c5c5de6e4612376c7eeb9e15c69aaf2324ab
fjyange <491854072@qq.com>
2020-05-24 21:43:29 +0800

    修改忽略文件

--- src/com/sozone/fs/common/Constant.java
+++ src/com/sozone/fs/common/Constant.java
@@ -12,8 +12,8 @@ public interface Constant {
 	String CITY_THEME = "CITY_THEME";
 
 	String SYS_SWICH = "on";
-	String WEB_URL ="http://www.hanayiqi.com:8090/";
-	String VIEW_URL = "http://www.hanayiqi.com/";
+	String WEB_URL ="http://8.129.183.175:8090/";
+	String VIEW_URL = "http://8.129.183.175/";
 	String ALI_URL = "alipayqr://platformapi/startapp?saId=10000007&qrcode=";
 	String ZZ_URL = "https://www.alipay.com/?appId=20000123&actionType=scan&biz_data=";
 	String INSER_ORDER = "http://47.115.93.230/authorize/order/in";
