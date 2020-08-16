914c338278b3c7d40a79dc6493dec924d4772119
fjyange <491854072@qq.com>
2020-08-16 20:53:22 +0800

    错单重提

--- src/com/sozone/fs/order/OrderAction.java
+++ src/com/sozone/fs/order/OrderAction.java
@@ -34,7 +34,8 @@ import com.sozone.fs.third.ThirdAction;
 
 @Path(value = "/order", desc = "订单处理")
 @Permission(Level.Authenticated)
-public class OrderAction {
+public class OrderAction
+{
 	/**
 	 * 持久化接口
 	 */
@@ -46,7 +47,8 @@ public class OrderAction {
 	 * @param activeRecordDAO
 	 *            the activeRecordDAO to set
 	 */
-	public void setActiveRecordDAO(ActiveRecordDAO activeRecordDAO) {
+	public void setActiveRecordDAO(ActiveRecordDAO activeRecordDAO)
+	{
 		this.activeRecordDAO = activeRecordDAO;
 	}
 
@@ -57,12 +59,14 @@ public class OrderAction {
 
 	@Path(value = "/findPage", desc = "获取订单列表")
 	@Service
-	public ResultVO<Page<Record<String, Object>>> findPage(AeolusData aeolusData) throws FacadeException {
+	public ResultVO<Page<Record<String, Object>>> findPage(AeolusData aeolusData) throws FacadeException
+	{
 		logger.debug(LogUtils.format("获取订单信息", aeolusData));
 		ResultVO<Page<Record<String, Object>>> resultVO = new ResultVO<>();
 		Record<String, Object> record = aeolusData.getRecord();
 		String searchTime = record.getString("SEARCH_TIME");
-		if (StringUtils.isNotBlank(searchTime)) {
+		if (StringUtils.isNotBlank(searchTime))
+		{
 			JSONArray jsonArray = record.getJSONArray("SEARCH_TIME");
 			record.setColumn("START_TIME", jsonArray.get(0));
 			record.setColumn("END_TIME", jsonArray.get(1));
@@ -75,17 +79,19 @@ public class OrderAction {
 		resultVO.setResult(page);
 		return resultVO;
 	}
-	
+
 	@Path(value = "/findHistory", desc = "获取订单列表")
 	@Service
-	public ResultVO<Page<Record<String, Object>>> findHistory(AeolusData aeolusData) throws FacadeException {
+	public ResultVO<Page<Record<String, Object>>> findHistory(AeolusData aeolusData) throws FacadeException
+	{
 		logger.debug(LogUtils.format("获取订单信息", aeolusData));
 		ResultVO<Page<Record<String, Object>>> resultVO = new ResultVO<>();
 		Record<String, Object> record = aeolusData.getRecord();
 		record.setColumn("USER_ID", ApacheShiroUtils.getCurrentUserID());
 		record.setColumn("IS_ADMIN", ApacheShiroUtils.getCurrentUser().getString("IS_ADMIN"));
 		String searchTime = record.getString("SEARCH_TIME");
-		if (StringUtils.isNotBlank(searchTime)) {
+		if (StringUtils.isNotBlank(searchTime))
+		{
 			JSONArray jsonArray = record.getJSONArray("SEARCH_TIME");
 			record.setColumn("START_TIME", jsonArray.get(0));
 			record.setColumn("END_TIME", jsonArray.get(1));
@@ -96,52 +102,79 @@ public class OrderAction {
 		resultVO.setResult(page);
 		return resultVO;
 	}
-	
-	@Path(value="/errorOrder",desc="错单重提")
+
+	@Path(value = "/errorOrder", desc = "错单重提")
 	@Service
-	public ResultVO<String> errorOrder(AeolusData aeolusData) throws FacadeException{
+	public ResultVO<String> errorOrder(AeolusData aeolusData) throws FacadeException
+	{
 		ResultVO<String> resultVO = new ResultVO<>();
 		Record<String, Object> record = aeolusData.getRecord();
 		String id = record.getString("ID");
-		Double money = record.getDouble("V_MONEY");
+		Double money = record.getDouble("V_PAY_TOTAL");
 		Record<String, Object> orderRecord = this.activeRecordDAO.pandora()
 				.SELECT_ALL_FROM(Constant.TableName.T_ORDER_TAB).EQUAL("ID", id).get();
 		String userID = orderRecord.getString("V_BELONG_USER");
 		if (StringUtils.equals("1", orderRecord.getString("V_STATUS"))
-				|| StringUtils.equals("3", orderRecord.getString("V_STATUS"))) {
+				|| StringUtils.equals("3", orderRecord.getString("V_STATUS")))
+		{
 			resultVO.setResult("订单已被确认，无法重提错单");
 			return resultVO;
 		}
 		Date date = DateUtils.parseDate(orderRecord.getString("V_CREATE_TIME"), "yyyy-MM-dd HH:mm:ss");
 		long outtime = date.getTime() + 5 * 58 * 1000;
-		if (outtime > System.currentTimeMillis()) {
+		if (outtime > System.currentTimeMillis())
+		{
 			resultVO.setSuccess(false);
 			resultVO.setResult("订单未过期，请稍后");
 			return resultVO;
 		}
 		Record<String, Object> bondRecord = this.activeRecordDAO.pandora()
 				.SELECT_ALL_FROM(Constant.TableName.T_BOND_TODAY).EQUAL("V_USER_ID", userID).get();
-		if (bondRecord.getDouble("V_SURPLUS_BOND") < money) {
+		if (bondRecord.getDouble("V_SURPLUS_BOND") < money)
+		{
 			resultVO.setResult("保证金额度不足，请充值");
 			return resultVO;
 		}
+
+		Record<String, Object> userRecord = this.activeRecordDAO.pandora()
+				.SELECT_ALL_FROM(Constant.TableName.T_SYS_USER_BASE)
+				.EQUAL("USER_ID", ApacheShiroUtils.getCurrentUserID()).get();
+		if (CollectionUtils.isEmpty(userRecord))
+		{
+			resultVO.setResult("当前登录用户不存在");
+			return resultVO;
+		}
+
+		String validKey = userRecord.getString("VALID_KEY");
+		if (StringUtils.isEmpty(validKey))
+		{
+			resultVO.setResult("无错单权限");
+			return resultVO;
+		}
+
+		if (!StringUtils.equals(validKey, record.getString("V_KEY")))
+		{
+			resultVO.setResult("验证秘钥错误");
+			return resultVO;
+		}
+
 		orderRecord.remove("ID");
 		orderRecord.remove("V_MONEY");
 		orderRecord.setColumn("ID", Random.generateUUID());
 		orderRecord.setColumn("V_MONEY", money);
 		orderRecord.setColumn("V_UPDATE_USER", ApacheShiroUtils.getCurrentUserID());
 		orderRecord.setColumn("V_UPDATE_TIME", DateUtils.getDateTime());
-		orderRecord.setColumn("V_CREATE_TIME", DateUtils.getDateTime());
 		orderRecord.setColumn("V_STATUS", "4");
 		this.activeRecordDAO.pandora().INSERT_INTO(Constant.TableName.T_ORDER_TAB).VALUES(orderRecord).excute();
 		resultVO.setSuccess(true);
 		resultVO.setResult("提交成功");
 		return resultVO;
 	}
-	
+
 	@Path(value = "/audit", desc = "订单审核")
 	@Service
-	public ResultVO<String> audit(AeolusData aeolusData) throws Exception {
+	public ResultVO<String> audit(AeolusData aeolusData) throws Exception
+	{
 		ResultVO<String> resultVO = new ResultVO<String>(false);
 		Record<String, Object> record = aeolusData.getRecord();
 		String id = record.getString("ID");
@@ -155,21 +188,27 @@ public class OrderAction {
 		String userID = orderRecord.getString("V_BELONG_USER");
 		String account = orderRecord.getString("V_BELONG_ACCOUNT");
 		double money = orderRecord.getDouble("V_MONEY");
-		try {
+		try
+		{
 			statefulDAO = new StatefulDAOImpl();
 			orderDao = new StatefulDAOImpl();
 			Record<String, Object> updateRecord = new RecordImpl<>();
 			if (StringUtils.equals("1", orderRecord.getString("V_STATUS"))
-					|| StringUtils.equals("3", orderRecord.getString("V_STATUS"))) {
+					|| StringUtils.equals("3", orderRecord.getString("V_STATUS")))
+			{
 				resultVO.setResult("订单已被确认，请刷新页面");
 				return resultVO;
 			}
-			if (StringUtils.equals("4", status) && !StringUtils.equals("2", orderRecord.getString("V_STATUS"))) {
+			if (StringUtils.equals("4", status) && !StringUtils.equals("2", orderRecord.getString("V_STATUS")))
+			{
 				updateRecord.setColumn("V_MONEY", String.valueOf(-money));
-				if (StringUtils.equals("01", payType)) {
+				if (StringUtils.equals("01", payType))
+				{
 					updateRecord.setColumn("ALI_MONEY", String.valueOf(-money));
 					updateRecord.setColumn("WX_MONEY", "0");
-				} else {
+				}
+				else
+				{
 					updateRecord.setColumn("ALI_MONEY", "0");
 					updateRecord.setColumn("WX_MONEY", String.valueOf(-money));
 				}
@@ -180,10 +219,13 @@ public class OrderAction {
 				updateRecord.setColumn("V_USER_ID", userID);
 				updateRecord.setColumn("SURPLUS_BOND", String.valueOf(money));
 				statefulDAO.statement().update("Order.updateUser", updateRecord);
-			} else if (StringUtils.equals("3", status)) {// 补单
+			}
+			else if (StringUtils.equals("3", status))
+			{// 补单
 				Record<String, Object> bondRecord = this.activeRecordDAO.pandora()
 						.SELECT_ALL_FROM(Constant.TableName.T_BOND_TODAY).EQUAL("V_USER_ID", userID).get();
-				if (bondRecord.getDouble("V_SURPLUS_BOND") < money) {
+				if (bondRecord.getDouble("V_SURPLUS_BOND") < money)
+				{
 					resultVO.setResult("保证金额度不足，请充值");
 					return resultVO;
 				}
@@ -195,10 +237,13 @@ public class OrderAction {
 				orderDao.commit();
 				updateRecord.setColumn("V_APP_ID", appID);
 				updateRecord.setColumn("V_MONEY", String.valueOf(money));
-				if (StringUtils.equals("01", payType)) {
+				if (StringUtils.equals("01", payType))
+				{
 					updateRecord.setColumn("ALI_MONEY", String.valueOf(money));
 					updateRecord.setColumn("WX_MONEY", "0");
-				} else {
+				}
+				else
+				{
 					updateRecord.setColumn("ALI_MONEY", "0");
 					updateRecord.setColumn("WX_MONEY", String.valueOf(money));
 				}
@@ -214,21 +259,27 @@ public class OrderAction {
 				updateRecord.clear();
 				updateRecord.setColumn("V_ACCOUNT_ID", account);
 				statefulDAO.statement().update("Order.updateJAccountTimes");
-			} else if (StringUtils.equals("1", status) && StringUtils.equals("2", record.getString("V_STATUS"))) {
+			}
+			else if (StringUtils.equals("1", status) && StringUtils.equals("2", record.getString("V_STATUS")))
+			{
 				resultVO.setSuccess(false);
 				resultVO.setResult("订单已过期，请联系客服进行补单");
 				return resultVO;
-			} else if (StringUtils.equals("1", status)) {// 确认
+			}
+			else if (StringUtils.equals("1", status))
+			{// 确认
 				Date date = DateUtils.parseDate(orderRecord.getString("V_CREATE_TIME"), "yyyy-MM-dd HH:mm:ss");
 				long outtime = date.getTime() + 5 * 58 * 1000;
-				if (outtime < System.currentTimeMillis()) {
+				if (outtime < System.currentTimeMillis())
+				{
 					resultVO.setSuccess(false);
 					resultVO.setResult("订单已过期，请联系客服进行补单");
 					return resultVO;
 				}
 				Record<String, Object> bondRecord = this.activeRecordDAO.pandora()
 						.SELECT_ALL_FROM(Constant.TableName.T_BOND_TODAY).EQUAL("V_USER_ID", userID).get();
-				if (bondRecord.getDouble("V_SURPLUS_BOND") < money) {
+				if (bondRecord.getDouble("V_SURPLUS_BOND") < money)
+				{
 					resultVO.setResult("保证金额度不足，请充值");
 					return resultVO;
 				}
@@ -240,10 +291,13 @@ public class OrderAction {
 				orderDao.commit();
 				updateRecord.setColumn("V_APP_ID", appID);
 				updateRecord.setColumn("V_MONEY", String.valueOf(money));
-				if (StringUtils.equals("01", payType)) {
+				if (StringUtils.equals("01", payType))
+				{
 					updateRecord.setColumn("ALI_MONEY", String.valueOf(money));
 					updateRecord.setColumn("WX_MONEY", "0");
-				} else {
+				}
+				else
+				{
 					updateRecord.setColumn("ALI_MONEY", "0");
 					updateRecord.setColumn("WX_MONEY", String.valueOf(money));
 				}
@@ -267,29 +321,37 @@ public class OrderAction {
 			optParams.setColumn("V_REQUEST_IP", getIp(aeolusData.getHttpServletRequest()));
 			statefulDAO.pandora().INSERT_INTO(Constant.TableName.T_ORDER_OPT).VALUES(optParams).excute();
 			statefulDAO.commit();
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			orderDao.rollback();
 			statefulDAO.rollback();
 			logger.error(LogUtils.format("审核失败", e.getMessage()), e);
 			resultVO.setResult("审核失败");
 			return resultVO;
-		} finally {
+		}
+		finally
+		{
 			statefulDAO.close();
 			orderDao.close();
 		}
-		if (StringUtils.equals("3", status) || StringUtils.equals("1", status)) {
+		if (StringUtils.equals("3", status) || StringUtils.equals("1", status))
+		{
 			Record<String, Object> appRecord = this.activeRecordDAO.pandora()
 					.SELECT_ALL_FROM(Constant.TableName.T_APP_TAB).EQUAL("ID", appID).get();
-			if (!CollectionUtils.isEmpty(appRecord)) {
+			if (!CollectionUtils.isEmpty(appRecord))
+			{
 				Record<String, String> sendRecord = new RecordImpl<>();
 				sendRecord.setColumn("orderno", orderRecord.getString("V_ORDER_NO"));
 				sendRecord.setColumn("money", orderRecord.getString("V_MONEY"));
 				sendRecord.setColumn("status", "1");
 				String signStr = ThirdAction.getSign(sendRecord, appRecord.getString("V_SECRET"));
 				sendRecord.setColumn("sign", signStr);
-				if (StringUtils.isNotBlank(orderRecord.getString("V_NOTIFY_URL"))) {
+				if (StringUtils.isNotBlank(orderRecord.getString("V_NOTIFY_URL")))
+				{
 					Record<String, Object> sendPar = new RecordImpl<>();
-					try {
+					try
+					{
 						sendPar.setColumn("ID", Random.generateUUID());
 						sendPar.setColumn("V_SEND_URL", orderRecord.getString("V_NOTIFY_URL"));
 						sendPar.setColumn("V_SEND_ORDER", id);
@@ -297,24 +359,34 @@ public class OrderAction {
 						String result = HttpClientUtils.sendJsonPostRequest(orderRecord.getString("V_NOTIFY_URL"),
 								JSONObject.toJSONString(sendRecord), "utf-8");
 						sendPar.setColumn("V_SEND_MSG", JSONObject.toJSONString(sendRecord));
-						if (StringUtils.equals("success", result)) {
+						if (StringUtils.equals("success", result))
+						{
 							sendPar.setColumn("V_SEND_STATUS", "success");
-						}else {
+						}
+						else
+						{
 							JSONObject jsonObject = JSONObject.parseObject(result);
-							if (StringUtils.equals("success", jsonObject.getString("success"))) {
+							if (StringUtils.equals("success", jsonObject.getString("success")))
+							{
 								sendPar.setColumn("V_SEND_STATUS", "success");
-							}else {
+							}
+							else
+							{
 								sendPar.setColumn("V_SEND_STATUS", "fail");
 							}
 						}
 						sendPar.setColumn("V_RETURN_MSG", result);
 						sendPar.setColumn("V_RETURN_TIME", System.currentTimeMillis());
-					} catch (Exception e) {
+					}
+					catch (Exception e)
+					{
 						sendPar.setColumn("V_SEND_STATUS", "fail");
 						sendPar.setColumn("V_RETURN_MSG", e.getMessage());
 						sendPar.setColumn("V_RETURN_TIME", System.currentTimeMillis());
 						logger.error(LogUtils.format("发送数据失败", e.getMessage()), e);
-					} finally {
+					}
+					finally
+					{
 						this.activeRecordDAO.pandora().INSERT_INTO(Constant.TableName.T_SEND_TAB).VALUES(sendPar)
 								.excute();
 					}
@@ -326,21 +398,29 @@ public class OrderAction {
 		return resultVO;
 	}
 
-	public String getIp(HttpServletRequest request) throws Exception {
+	public String getIp(HttpServletRequest request) throws Exception
+	{
 		String ip = request.getHeader("X-Forwarded-For");
-		if (ip != null) {
-			if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip)) {
+		if (ip != null)
+		{
+			if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip))
+			{
 				int index = ip.indexOf(",");
-				if (index != -1) {
+				if (index != -1)
+				{
 					return ip.substring(0, index);
-				} else {
+				}
+				else
+				{
 					return ip;
 				}
 			}
 		}
 		ip = request.getHeader("X-Real-IP");
-		if (ip != null) {
-			if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip)) {
+		if (ip != null)
+		{
+			if (!ip.isEmpty() && !"unKnown".equalsIgnoreCase(ip))
+			{
 				return ip;
 			}
 		}
